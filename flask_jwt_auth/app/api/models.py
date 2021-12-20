cf760158149f4065123b8092393aa44fb4c5d761
Bryce Becker <bryce.becker15@gmail.com>
2021-12-20 16:08:12 -0500

    PEP formatting
    Minor syntax and styling corrections according to PEP

--- flask_jwt_auth/app/api/models.py
+++ flask_jwt_auth/app/api/models.py
@@ -5,9 +5,9 @@ class User(db.Model):
     """
     Maps user object for SQLite db
     """
-    id = db.Column(db.Integer, primary_key = True)
-    public_id = db.Column(db.String(50), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(100))
-    email = db.Column(db.String(70), unique = True, index = True)
+    email = db.Column(db.String(70), unique=True, index=True)
     password = db.Column(db.String(80))
-    active_jwt = db.Column(db.String(350), unique = True)
+    active_jwt = db.Column(db.String(350), unique=True)
--- flask_jwt_auth/app/api/routes.py
+++ flask_jwt_auth/app/api/routes.py
@@ -1,17 +1,17 @@
 import uuid
 from flask import request, jsonify, make_response, Blueprint
 from flask_jwt_extended import (
-        create_access_token, get_jwt_identity, jwt_required,
-        set_access_cookies, unset_jwt_cookies
-        )
+    create_access_token, get_jwt_identity, jwt_required,
+    set_access_cookies, unset_jwt_cookies
+)
 from . import db, bcrypt
 from .models import User
 
 
 auth = Blueprint('auth', __name__)
 
-  
-@auth.route('/signup', methods =['POST'])
+
+@auth.route('/signup', methods=['POST'])
 def signup():
     """
     Recieves form input and creates user, if it doesn't exist
@@ -20,19 +20,19 @@ def signup():
     data = request.form
     name, email = data.get('name'), data.get('email')
     password = data.get('password')
-  
+
     # checking for existing user
-    user = User.query.filter_by(email = email).first()
+    user = User.query.filter_by(email=email).first()
 
     if not user:
         user = User(
-            public_id = str(uuid.uuid4()),
-            name = name,
-            email = email,
-            password = bcrypt.generate_password_hash(password),
-            active_jwt = None
+            public_id=str(uuid.uuid4()),
+            name=name,
+            email=email,
+            password=bcrypt.generate_password_hash(password),
+            active_jwt=None
         )
-        
+
         db.session.add(user)
         db.session.commit()
 
@@ -42,37 +42,37 @@ def signup():
         return make_response('User already exists. Please Log in.', 202)
 
 
-@auth.route('/login', methods =['POST'])
+@auth.route('/login', methods=['POST'])
 def login():
     """
     Verifies credentials and creates JWT, if valid user
     """
     # creates dictionary of form data
     auth = request.form
-  
+
     if not auth or not auth.get('email') or not auth.get('password'):
         # returns 401 if any email and/or password is missing
         return make_response('Could not verify', 401)
-  
-    user = User.query.filter_by(email = auth.get('email')).first()
-  
+
+    user = User.query.filter_by(email=auth.get('email')).first()
+
     if not user:
         # returns 401 if user does not exist
         return make_response('Could not verify', 401)
-  
+
     if bcrypt.check_password_hash(user.password, auth.get('password')):
         # generates the JWT, sets in db
-        token = create_access_token(identity=user.email) # should be public_id
+        token = create_access_token(identity=user.email)  # should be public_id
         user.active_jwt = token
         db.session.commit()
 
-        return make_response(jsonify({'token' : token}), 201)
+        return make_response(jsonify({'token': token}), 201)
 
     # returns 403 if password is wrong
     return make_response('Could not verify', 403)
 
 
-@auth.route('/user', methods =['GET'])
+@auth.route('/user', methods=['GET'])
 @jwt_required()
 def get_all_users():
     """
@@ -84,10 +84,10 @@ def get_all_users():
     for user in users:
         output.append({
             'public_id': user.public_id,
-            'name' : user.name,
-            'email' : user.email
+            'name': user.name,
+            'email': user.email
         })
-  
+
     return jsonify({'users': output})
 
 
@@ -97,9 +97,8 @@ def whoami():
     """
     Requires JWT auth and returns user's identity based on token
     """
-    # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
-    return make_response(jsonify({'logged_in_as' : current_user}), 200)
+    return make_response(jsonify({'logged in as': current_user}), 200)
 
 
 @auth.route('/login_with_cookie', methods=['POST'])
@@ -109,17 +108,17 @@ def login_with_cookie():
     """
     # creates dictionary of form data
     auth = request.form
-  
+
     if not auth or not auth.get('email') or not auth.get('password'):
         # returns 401 if any email and/or password is missing
         return make_response('Could not verify', 401)
-  
-    user = User.query.filter_by(email = auth.get('email')).first()
-  
+
+    user = User.query.filter_by(email=auth.get('email')).first()
+
     if not user:
         # returns 401 if user does not exist
         return make_response('Could not verify', 401)
-  
+
     if bcrypt.check_password_hash(user.password, auth.get('password')):
         # generates the JWT, sets in db
         token = create_access_token(identity=user.email)
@@ -140,14 +139,14 @@ def logout_with_cookie():
     """
     Mock logout using JWT cookie unset
     """
-    #regen public_id for user
+    # regen public_id for user
     current_user = get_jwt_identity()
-    user = User.query.filter_by(email = current_user).first()
+    user = User.query.filter_by(email=current_user).first()
     user.public_id = str(uuid.uuid4())
     user.active_jwt = None
     db.session.commit()
-    
-    #unset JWT cookie
+
+    # unset JWT cookie
     response = jsonify({'user': user.email, 'msg': 'logout successful'})
     unset_jwt_cookies(response)
 
